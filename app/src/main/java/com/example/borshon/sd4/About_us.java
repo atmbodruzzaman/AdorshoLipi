21f005ba28e845e68a8a73ba293aee896a405a65
atmbodruzzaman <atmbodruzzaman@gmail.com>
2020-09-03 03:08:44 +0600

    Adorsho Lipi

--- app/src/main/java/com/example/borshon/sd4/About_us.java
+++ app/src/main/java/com/example/borshon/sd4/About_us.java
@@ -9,5 +9,7 @@ public class About_us extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        setTitle("Design and Developed By: ");
     }
 }
